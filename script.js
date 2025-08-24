09a5d1a32c0e54db7e2101637ccce831d66168a4
hpregionalcouncil <rickastleyjr8@gmail.com>
2025-08-24 23:38:32 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const map = L.map('map', {
 
 // Step 2: Define image bounds
 const bounds = [[0,0], [1000,1000]]; // Example dimensions. Use your image's pixel size.
-const image = L.imageOverlay('path/to/your/image.jpg', bounds).addTo(map);
-
+const image = L.imageOverlay('my-map-image.jpg', bounds).addTo(map);
 // Step 3: Set initial view to fit the image
 map.fitBounds(bounds);
