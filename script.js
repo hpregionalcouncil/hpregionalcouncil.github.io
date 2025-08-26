86c955f5413370fd31bbea256f6962e57e5e7f08
hpregionalcouncil <rickastleyjr8@gmail.com>
2025-08-26 21:54:52 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const map = L.map('map', {
 });
 
 // Step 2: Define image bounds
-const bounds = [[0,0], [1080,1920]]; // Note: bounds are [Y,X] or [height, width]
+const bounds = [[0,0], [1080,1920]]; // Swapped to [height, width]
 const image = L.imageOverlay('map002.jpg', bounds).addTo(map);
 
-// Corrected marker declarations with unique variable names
-const point1 = L.marker([941,289]).addTo(map);
+// Corrected marker declarations with flipped coordinates and unique variable names
+const point1 = L.marker([289, 941]).addTo(map);
 point1.bindPopup("Diggings");
 
-const point2 = L.marker([1100, 100]).addTo(map);
+const point2 = L.marker([100, 1100]).addTo(map);
 point2.bindPopup("Yondanarra");
 
-const point3 = L.marker([800, 600]).addTo(map);
+const point3 = L.marker([600, 800]).addTo(map);
 point3.bindPopup("Ginninendi");
 
-const point4 = L.marker([800, 125]).addTo(map);
+const point4 = L.marker([125, 800]).addTo(map);
 point4.bindPopup("Lowe");
 
-const point5 = L.marker([1110, 500]).addTo(map);
+const point5 = L.marker([500, 1110]).addTo(map);
 point5.bindPopup("Blewy Hill");
 
 // Step 3: Set initial view to fit the image
