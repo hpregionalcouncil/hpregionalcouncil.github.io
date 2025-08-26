df6845057ce953d921ab0324a6d79a01f1e91a63
hpregionalcouncil <rickastleyjr8@gmail.com>
2025-08-26 21:48:05 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const map = L.map('map', {
 });
 
 // Step 2: Define image bounds
-const bounds = [[0,0], [1000,1000]]; // Example dimensions. Use your image's pixel size.
+const bounds = [[0,0], [1920,1080]]; // Example dimensions. Use your image's pixel size.
 const image = L.imageOverlay('map002.jpg', bounds).addTo(map);
 
 const point1 = L.marker([941,289]).addTo(map);
